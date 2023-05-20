e468a9a1473573eed008d80a43b55790a6805d1a
Pichardo098 <jesus098856@gmail.com>
2023-05-19 23:33:46 -0600

    Finalized

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {getRandom} from './utils/random'
 import QuoteBox from './Components/QuoteBox'
 import Button from './Components/Button'
 
+
 const bkgImages = [
   {
     "Space":'space1',
@@ -27,13 +28,13 @@ function App() {
   
   //Lógica Random Phrase
   const [quote, setQuote] = useState(getRandom(dbQuotes))
+
   const [bgImages, setBgIMages] = useState(getRandom(bkgImages))
   
 
   const handleChangeQuote = () => {
-    console.log('Hola');
-    //setQuote(getRandom(dbQuotes))
-    //setBgIMages(getRandom(bkgImages))
+    setQuote(getRandom(dbQuotes))
+    setBgIMages(getRandom(bkgImages))
   }
 
   return (
@@ -41,7 +42,7 @@ function App() {
       <main className={bgImages.Space}>
         <h1 className='principal_title'>INFO GALAXY</h1>
         <QuoteBox quote={quote}/>
-        <Button/>
+        <button onClick={handleChangeQuote} id='buton_change_Info'>Change Info</button>
 
         <footer>
           <div>
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,4 +1,3 @@
-
 const Button = ({handleChangeQuote}) => {
 
   return (
